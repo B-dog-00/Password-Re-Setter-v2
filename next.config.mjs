2f0215a09ab4429c33e5c029ad3f8134a377bb0c
B-dog-00 <aaronblain13@gmail.com>
2024-03-20 19:47:19 +0000

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/new-password-resetter',
-  assetPrefix: '/new-password-resetter/',
-  output: "export",
   reactStrictMode: true,
 };
 
